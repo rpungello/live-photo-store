196d2784f0b5633c77b98d810f1dbec84364b4cc
Rob Pungello <rob@rpun.io>
2026-08-20 08:02:47 -0400

    Resolve phpstan issue with Event.php

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -7,7 +7,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Carbon;
 
+/**
+ * @property Carbon $date
+ */
 class Event extends Model
 {
     /** @use HasFactory<EventFactory> */
